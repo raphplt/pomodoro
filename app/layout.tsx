40286de94ee12619df43dd60f9fbf5890a5835c8
Raphael <plassa_r@etna-alternance.net>
2024-01-07 14:56:47 +0100

    Disable auto CSS addition for FontAwesome icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+
 config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
